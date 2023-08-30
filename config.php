d09fb30d818c2e1fca2871bba71f052f31d7d8fe
Simon Vandereecken <simon@nithou.net>
2023-08-30 08:26:54 +0200

    Reordered config
    Reordered config from most chance of changes to least

--- config.php
+++ config.php
@@ -3,20 +3,10 @@
     $BLOG_TITLE = 'The Title'; // Replace by your blog title
     $BLOG_TAGLINE = 'your blog tagline, goes after the title'; // Replace by your blog tagline or leave empty
     $BLOG_DESCRIPTION = 'your blog description'; // Will be shown when sharing links from your blog
-    $LANG = 'en'; // Two code language of your blog lang
-
-    // For the lil' texts
-    $LINKTO = 'direct link'; // Displayed at the bottom of the posts
-    $BACKTO = 'back to the list'; // Displayed when on a post or page 
-    $HOME = 'home'; // Back to the home
 
     // Mainly used for RSS
     $BLOG_AUTHOR = 'your name'; // Your name or nickname
     $AUTHOR_EMAIL = 'your email'; // Your email
-
-    // Webmentions system to display external reactions see https://webmention.io/
-    $WEBMENTIONS = '';
-    $PINGBACK = '';
 
     // Here you can change the colors, some good ideas here http://a11yrocks.com/colorPalette/ and here https://www.randoma11y.com/
     // Light Mode colors
@@ -28,6 +18,16 @@
     $DARK_BACKGROUND = '#1E4147'; // Background for dark mode
     $DARK_TEXT = '#FAE3B4'; // Color of the text for dark mode
     $DARK_LINKS = '#AAC789'; // Color of the links when hovering for dark mode
+
+    // Webmentions system to display external reactions see https://webmention.io/
+    $WEBMENTIONS = '';
+    $PINGBACK = '';
+
+    // If your blog isn't in English
+    $LANG = 'en'; // Two code language of your blog lang
+    $LINKTO = 'direct link'; // Displayed at the bottom of the posts
+    $BACKTO = 'back to the list'; // Displayed when on a post or page 
+    $HOME = 'home'; // Back to the home
 
     // Here you can change how titles & texts look see https://developer.mozilla.org/en-US/docs/Web/CSS/font-family
     $TITLES = '"Helvetica Neue", "Arial Nova", Helvetica, "Liberation Sans", "Roboto", Arial, "Hiragino Sans GB", "Microsoft YaHei", "WenQuanYi Micro Hei", "Mplus 1p", "Hiragino Sans", "Hiragino Kaku Gothic Pro", "游ゴシック" , "游ゴシック体" , YuGothic , "Yu Gothic", "ＭＳ ゴシック" , "MS Gothic", system-ui, sans-serif'; // Default fonts for the titles

93ef359a45f2c0789d89b9a3eaa10051d8850712
Simon Vandereecken <simon@nithou.net>
2023-09-05 17:26:38 +0200

    Update footer.php
    Added button to load the archive page

--- footer.php
+++ footer.php
@@ -1,6 +1,14 @@
     </section>
 </main>
 
+<?php if (stripos($_SERVER['REQUEST_URI'], 'index.php'))
+    {
+    echo '<div id="more"><a class="more" href="./archive.php">'.$MOREPOSTS.'</a></div>';
+    } 
+    else {
+      echo '';
+};?>
+
 <footer>
     <!-- Have a nice day -->
 </footer>

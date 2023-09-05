94eb76e0a0e191ffdd9c333548d44abc5bf464f1
Simon Vandereecken <simon@nithou.net>
2023-09-05 17:27:00 +0200

    Archive page
    Displays all the pots

---
+++ archive.php
@@ -0,0 +1,31 @@
+<?php include 'header.php';?>
+<section class="list"> <!-- article list -->
+      
+      <?php
+        $files = glob("posts/*.md");
+        rsort($files);
+        foreach($files as $post) {
+          $link_id = basename($post , ".md");
+          $post = file_get_contents($post);
+          $title = implode("\n", array_slice(explode("\n", $post), 0, 1));
+          $title = substr($title, 2);
+          $summary = implode("\n", array_slice(explode("\n", $post), 1, 3));
+          $Parsedown = new ParsedownExtra();
+
+          if ($SHOW_SUMMARY === TRUE) {
+            echo '<article>';
+            echo '<h1>'.$title.'</h1>';
+            echo $summary;
+            echo '<a href="single.php?id='.$link_id.'" class="permalink">'.$LINKTO.'</a>';
+            echo '</article>';
+          } else {
+            echo '<article>';
+            echo $Parsedown->text($post);
+            echo '<a href="single.php?id='.$link_id.'" class="permalink">'.$LINKTO.'</a>';
+            echo '</article>';
+          };
+        };
+      ?>
+</section>
+
+<?php include 'footer.php';?>

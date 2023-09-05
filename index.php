960bf8fe9ce1921ad12641b196fd41474a23e300
Simon Vandereecken <simon@nithou.net>
2023-09-05 17:27:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
       <?php
         $files = glob("posts/*.md");
         rsort($files);
+        $counter = 0;
         foreach($files as $post) {
           $link_id = basename($post , ".md");
           $post = file_get_contents($post);
@@ -11,21 +12,26 @@
           $title = substr($title, 2);
           $summary = implode("\n", array_slice(explode("\n", $post), 1, 3));
           $Parsedown = new ParsedownExtra();
-
+          if ($counter == $POST_LIMIT) {
+            break;
+          }
           if ($SHOW_SUMMARY === TRUE) {
             echo '<article>';
             echo '<h1>'.$title.'</h1>';
             echo $summary;
             echo '<a href="single.php?id='.$link_id.'" class="permalink">'.$LINKTO.'</a>';
             echo '</article>';
+            $counter++;
           } else {
             echo '<article>';
             echo $Parsedown->text($post);
             echo '<a href="single.php?id='.$link_id.'" class="permalink">'.$LINKTO.'</a>';
             echo '</article>';
+            $counter++;
           };
         };
       ?>
-</section>
 
+      
+</section>
 <?php include 'footer.php';?>

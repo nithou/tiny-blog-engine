5fa4dde83f5a8a7f022e885fc27f56ce11be373c
Simon Vandereecken <simon@nithou.net>
2023-09-06 17:58:08 +0200

    Update kudos.php

--- kudos.php
+++ kudos.php
@@ -1,3 +1,4 @@
+<div id="kudos">
 <?php  
   if(isset($_POST['kudos'])) {
       function getCounter($counterID) {
@@ -13,10 +14,7 @@
          fclose($fil);           // Close the file
          return $numVisitors;    // Return the new countÒ
       }
-      echo '<div id="kudos">';
-      echo  '<span class="counter">'.getCounter("$id").'</span>';
-      echo '<form method="post"><button type="submit" name="kudos" value="kudos"/><svg class="heart" width="24" height="24" viewBox="0 0 24 24"><path d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z"></path></svg></button></form>';
-      echo '</div>';
+      echo '<span class="counter">'.getCounter("$id").'</span>';  
   } else {
       function getCounter($counterID) {
          $fileName = "posts/kudos/".$counterID."-count.txt"; 
@@ -30,9 +28,15 @@
          fclose($fil);           // Close the file
          return $numVisitors;    // Return the new countÒ
       }
-      echo '<div id="kudos">';
-      echo  '<span class="counter">'.getCounter("$id").'</span>';
-      echo '<form method="post"><button type="submit" name="kudos" value="kudos"/><svg class="heart" width="24" height="24" viewBox="0 0 24 24"><path d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z"></path></svg></button></form>';
-      echo '</div>';
+      echo '<span class="counter">'.getCounter("$id").'</span>';
   };
 ?>
+<form method="post">
+  <button type="submit" name="kudos" value="kudos"/>
+    <svg class="heart" width="24" height="24" viewBox="0 0 24 24">
+      <path d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z">        
+      </path>
+    </svg>
+  </button>
+</form>
+</div>

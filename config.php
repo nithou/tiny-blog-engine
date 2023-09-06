2ded97afdf850a7d23a251f448c51fe9db8e45e6
Simon Vandereecken <simon@nithou.net>
2023-09-06 18:30:22 +0200

    Update config.php
    Reordered config

--- config.php
+++ config.php
@@ -1,14 +1,16 @@
 <?php
+    // Blog informations
     $BLOG_LINK = 'https://example.com/'; // The address of your blog ending by a / like: https://example.com/
     $BLOG_TITLE = 'The Title'; // Replace by your blog title
     $BLOG_TAGLINE = 'your blog tagline, goes after the title'; // Replace by your blog tagline or leave empty
     $BLOG_DESCRIPTION = 'your blog description'; // Will be shown when sharing links from your blog
 
-    // Mainly used for RSS
+    // Author informations
     $BLOG_AUTHOR = 'your name'; // Your name or nickname
     $AUTHOR_EMAIL = 'your email'; // Your email
     $AUTHOR_DESCRIPTION = 'your description'; // Will be displayed at the bottom of the post. Upload an author.jpg into the /assets/img folder
 
+    // General Styling
     // Here you can change the colors, some good ideas here http://a11yrocks.com/colorPalette/ and here https://www.randoma11y.com/
     // Light Mode colors
     $LIGHT_BACKGROUND = '#FAE3B4'; // Background for light mode
@@ -20,30 +22,32 @@
     $DARK_TEXT = '#FAE3B4'; // Color of the text for dark mode
     $DARK_LINKS = '#AAC789'; // Color of the links when hovering for dark mode
 
-    // Webmentions system to display external reactions see https://webmention.io/
-    $WEBMENTIONS = false;
-    $WEBMENTIONSLINK = '';
-    $PINGBACK = '';
+    // Here you can change how titles & texts look see https://developer.mozilla.org/en-US/docs/Web/CSS/font-family
+    $TITLES = '"Helvetica Neue", "Arial Nova", Helvetica, "Liberation Sans", "Roboto", Arial, "Hiragino Sans GB", "Microsoft YaHei", "WenQuanYi Micro Hei", "Mplus 1p", "Hiragino Sans", "Hiragino Kaku Gothic Pro", "游ゴシック" , "游ゴシック体" , YuGothic , "Yu Gothic", "ＭＳ ゴシック" , "MS Gothic", system-ui, sans-serif'; // Default fonts for the titles
+    $TEXTS = 'Cambria, "Hoefler Text", Utopia, "Liberation Serif", "Nimbus Roman No9 L Regular", Times, "Times New Roman", "Hiragino Sans GB", "Microsoft YaHei", "WenQuanYi Micro Hei", "Mplus 1p", "Hiragino Sans", "Hiragino Kaku Gothic Pro", "游ゴシック" , "游ゴシック体" , YuGothic , "Yu Gothic", "ＭＳ ゴシック" , "MS Gothic", serif'; // Default fonts for the texts
 
-    // Add Commento support
-    $COMMENTO = false;
-    $COMMENTO_URL = 'https://cdn.commento.io/js/commento.js';
-
-    // Add Kudos support
-    $KUDOS = true;
-
-    // If your blog isn't in English
+    // Language 
     $LANG = 'en'; // Two code language of your blog lang
     $LINKTO = 'direct link'; // Displayed at the bottom of the posts
     $BACKTO = 'back to the list'; // Displayed when on a post or page 
     $HOME = 'home'; // Back to the home
     $MOREPOSTS = 'see all posts';  // Text that links to the archive page
 
-    // Only show summary in home
+    // Homepage display
     $SHOW_SUMMARY = false; // Change to true if you want to only display a short summary in home
     $POST_LIMIT = 10; // Limit the amount of posts you want to display on the homepage
 
-    // Here you can change how titles & texts look see https://developer.mozilla.org/en-US/docs/Web/CSS/font-family
-    $TITLES = '"Helvetica Neue", "Arial Nova", Helvetica, "Liberation Sans", "Roboto", Arial, "Hiragino Sans GB", "Microsoft YaHei", "WenQuanYi Micro Hei", "Mplus 1p", "Hiragino Sans", "Hiragino Kaku Gothic Pro", "游ゴシック" , "游ゴシック体" , YuGothic , "Yu Gothic", "ＭＳ ゴシック" , "MS Gothic", system-ui, sans-serif'; // Default fonts for the titles
-    $TEXTS = 'Cambria, "Hoefler Text", Utopia, "Liberation Serif", "Nimbus Roman No9 L Regular", Times, "Times New Roman", "Hiragino Sans GB", "Microsoft YaHei", "WenQuanYi Micro Hei", "Mplus 1p", "Hiragino Sans", "Hiragino Kaku Gothic Pro", "游ゴシック" , "游ゴシック体" , YuGothic , "Yu Gothic", "ＭＳ ゴシック" , "MS Gothic", serif'; // Default fonts for the texts
+    // Comments & Reactions systems
+
+    // Kudos support
+    $KUDOS = true;
+
+    // Webmentions system to display external reactions see https://webmention.io/
+    $WEBMENTIONS = false;
+    $WEBMENTIONSLINK = '';
+    $PINGBACK = '';
+
+    // Commento support
+    $COMMENTO = false;
+    $COMMENTO_URL = 'https://cdn.commento.io/js/commento.js';
 ?>

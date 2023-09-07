e682e55a0acdf1581e168d4303c3f0b89975e073
Simon Vandereecken <simon@nithou.net>
2023-09-07 11:17:11 +0200

    Update page.php
    cleaning

--- page.php
+++ page.php
@@ -9,18 +9,7 @@
    echo $Parsedown->text($html);
    echo '&#8249; <a href="'.$BLOG_LINK.'">'.$BACKTO.'</a>';
    echo '</article>';
-?>
-
-<!-- FULL BRID.GY FOR WEBMENTIONS SUPPORT -->
- <?php if ($WEBMENTIONS === TRUE) {
-echo '<a href="https://brid.gy/publish/flickr" rel="webmention"></a>
-<a href="https://brid.gy/publish/github" rel="webmention"></a>
-<a href="https://brid.gy/publish/mastodon" rel="webmention"></a>
-<a href="https://brid.gy/publish/twitter" rel="webmention"></a>
-<data class="p-bridgy-omit-link" value="false"></data>
-<div id="webmentions"></div>';} else {};?>
-<!-- EOF BRID.GY -->
-   
+?> 
 </section>
 
 <?php include 'parts/footer.php';?>

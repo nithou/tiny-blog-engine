ecb581a4fe5958a936db5bbedecf5717a12e0c5d
Simon Vandereecken <simon@nithou.net>
2023-10-15 13:09:24 +0200

    Bye AI
    By default block IA requests

--- parts/header.php
+++ parts/header.php
@@ -48,6 +48,7 @@ $themeVars = [
     <meta charset="UTF-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="robots" content="noai, noimageai">
     <meta property="og:locale" content="en_GB"/>
     <meta property="og:image" content="<?php echo $BLOG_LINK; ?>assets/img/og.png"/>
     <meta name="author" content="<?php echo $BLOG_AUTHOR; ?>">
